514142219813ad97e269821edeb66fc7393a7889
Gabor Meszaros <gabor.meszaros@logmein.com>
2015-03-02 16:30:07 +0100

    Creating script builder

---
+++ services/database/sqlBuilder.js
@@ -0,0 +1,9 @@
+/**
+ * Created by gmeszaros on 3/2/2015.
+ */
+var stringBuilder = require("stringbuilder");
+module.exports = function () {
+    this.createInsert = function (table, columns, values) {
+
+    };
+};
